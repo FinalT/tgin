f6f4d3e2b796503c0c7033cfe960801db1305e42
FinalT <136178931@qq.com>
2023-02-16 12:28:10 +0800

    为context封装数据的转换功能

--- server_e2e_test.go
+++ server_e2e_test.go
@@ -1,6 +1,7 @@
 package tgin
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -13,6 +14,13 @@ func TestServer(t *testing.T) {
 	})
 	s.Get("/user", func(ctx *Context) {
 		ctx.Resp.Write([]byte("hello, user"))
+	})
+
+	s.Post("/form", func(ctx *Context) {
+		err := ctx.Req.ParseForm()
+		if err != nil {
+			fmt.Println(err)
+		}
 	})
 
 	s.Start(":8081")
